22a13a54f56652de42a0978173672414a3db82ca
STARK404 <134937814+STARK-404@users.noreply.github.com>
2023-06-06 21:34:59 +0530

    Add files via upload

---
+++ insta.py
@@ -0,0 +1,11 @@
+import instaloader
+import os
+# Get instance
+L = instaloader.Instaloader()
+
+# Optionally, login or load session
+USER = "cpane_lh"
+PASSWORD = "stark@123"
+L.login(USER, PASSWORD)# (login)
+# (ask password on terminal)
+# (load session created w/
